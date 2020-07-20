1248ac0887c4edd0d98f2658531d2213f8ddec3b
Davone <davone.ford@gmail.com>
2020-07-19 17:52:26 -0700

    python standards

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ LOG.setLevel(logging.INFO)
 def scale(payload):
     """Scales Payload"""
     
-    LOG.info("Scaling Payload: \n{payload}")
+    LOG.info(f"<h>Scaling Payload: \n{payload}")
     scaler = StandardScaler().fit(payload.astype(float))
     scaled_adhoc_predict = scaler.transform(payload.astype(float))
     return scaled_adhoc_predict
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home</h3>"
+    html = "<h3>Sklearn Prediction Home</h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
@@ -56,7 +56,7 @@ def predict():
     
     # Logging the input payload
     json_payload = request.json
-    LOG.info("JSON payload: \n{json_payload}")
+    LOG.info(f"JSON payload: \n{json_payload}")
     inference_payload = pd.DataFrame(json_payload)
     LOG.info(f"Inference payload DataFrame: \n{inference_payload}")
     # scale the input
@@ -64,7 +64,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
-    LOG.info(f"Prediction %s", jsonify({'prediction': prediction}))
+    LOG.info("Prediction %s", jsonify({'prediction': prediction}))
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
